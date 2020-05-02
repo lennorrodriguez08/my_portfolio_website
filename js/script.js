b5452fe43ad8ebbb905465c3e9a4051ab3376194
Ronnel Rodriguez <41136127+lennorrodriguez08@users.noreply.github.com>
2020-05-02 21:31:58 +0800

    Add files via upload
    initial commit

---
+++ js/script.js
@@ -0,0 +1,96 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // KNOW MORE BUTTON
+    document.querySelector(".tag").addEventListener("mouseover", function(e) {
+        document.querySelector(".fa-angle-double-right").style.transform = "rotate(90deg)";
+    });
+
+    // KNOW MORE BUTTON
+    document.querySelector(".tag").addEventListener("mouseleave", function(e) {
+        document.querySelector(".fa-angle-double-right").style.transform = "rotate(0deg)";
+    });
+
+    // PROJECTS
+
+    // PROJECT 1
+    document.querySelector(".project-1").addEventListener("mouseover", function(e) {
+        document.querySelector(".pop-1").style.opacity = "1";
+    })
+
+    document.querySelector(".project-1").addEventListener("mouseleave", function(e) {
+        document.querySelector(".pop-1").style.opacity = "0";
+    })
+
+    // PROJECT 2
+    document.querySelector(".project-2").addEventListener("mouseover", function(e) {
+        document.querySelector(".pop-2").style.opacity = "1";
+    })
+
+    document.querySelector(".project-2").addEventListener("mouseleave", function(e) {
+        document.querySelector(".pop-2").style.opacity = "0";
+    })
+
+    // PROJECT 3
+    document.querySelector(".project-3").addEventListener("mouseover", function(e) {
+        document.querySelector(".pop-3").style.opacity = "1";
+    })
+
+    document.querySelector(".project-3").addEventListener("mouseleave", function(e) {
+        document.querySelector(".pop-3").style.opacity = "0";
+    })
+
+    // PROJECT 4
+    document.querySelector(".project-4").addEventListener("mouseover", function(e) {
+        document.querySelector(".pop-4").style.opacity = "1";
+    })
+
+    document.querySelector(".project-4").addEventListener("mouseleave", function(e) {
+        document.querySelector(".pop-4").style.opacity = "0";
+    })
+
+    // PROJECT 5
+    document.querySelector(".project-5").addEventListener("mouseover", function(e) {
+        document.querySelector(".pop-5").style.opacity = "1";
+    })
+
+    document.querySelector(".project-5").addEventListener("mouseleave", function(e) {
+        document.querySelector(".pop-5").style.opacity = "0";
+    })
+
+    // PROJECT 6
+    document.querySelector(".project-6").addEventListener("mouseover", function(e) {
+        document.querySelector(".pop-6").style.opacity = "1";
+    })
+
+    document.querySelector(".project-6").addEventListener("mouseleave", function(e) {
+        document.querySelector(".pop-6").style.opacity = "0";
+    })
+
+    // PROJECT 7
+    document.querySelector(".project-7").addEventListener("mouseover", function(e) {
+        document.querySelector(".pop-7").style.opacity = "1";
+    })
+
+    document.querySelector(".project-7").addEventListener("mouseleave", function(e) {
+        document.querySelector(".pop-7").style.opacity = "0";
+    })
+
+    // PROJECT 8
+    document.querySelector(".project-8").addEventListener("mouseover", function(e) {
+        document.querySelector(".pop-8").style.opacity = "1";
+    })
+
+    document.querySelector(".project-8").addEventListener("mouseleave", function(e) {
+        document.querySelector(".pop-8").style.opacity = "0";
+    })
+
+    // PROJECT 9
+    document.querySelector(".project-9").addEventListener("mouseover", function(e) {
+        document.querySelector(".pop-9").style.opacity = "1";
+    })
+
+    document.querySelector(".project-9").addEventListener("mouseleave", function(e) {
+        document.querySelector(".pop-9").style.opacity = "0";
+    })
+    
+
+});
